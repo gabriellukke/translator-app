7f79a7227ce57713e90fc77cca43ee2a417f5c51
gabriellukke <gabriellukke@outlook.com>
2022-10-07 21:00:38 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex w-screen h-16 bg-white text-3xl text-ballblue justify-center items-center">
       <h1>Tradutor</h1>
     </header>
   );
