a52d2595cd442a7bd5c94292f41a220c14bfdb23
gabriellukke <gabriellukke@outlook.com>
2022-10-12 19:59:51 -0300

    feat: use debounce hook

---
+++ src/hooks/useDebouncer.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export default function useDebounce(value: string, delay: number) {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+}
