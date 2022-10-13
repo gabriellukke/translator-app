d0e704ad1404e3f4a8cbded909dabadd99f31719
gabriellukke <gabriellukke@outlook.com>
2022-10-12 23:06:43 -0300

    style: textarea

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -4,7 +4,7 @@ import { TextAreaProps } from './types';
 export default function TextArea({ value, onChange }: TextAreaProps) {
   return (
     <textarea
-      className="w-1/2 h-1/2 bg-white text-ballblue"
+      className="w-1/2 h-1/2 p-2 bg-white shadow-lg rounded-tl-lg rounded-bl-lg text-2xl resize-none"
       placeholder="Digite aqui"
       value={value}
       onChange={(e) => onChange(e.target.value)}
