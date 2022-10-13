ec98ecbfad1af0e017191f47868ad865462876c1
gabriellukke <gabriellukke@outlook.com>
2022-10-12 22:10:15 -0300

    style: display

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { DisplayProps } from './types';
 
-export default function Display({ text, className }: DisplayProps) {
-  return <div className={className}>{text}</div>;
+export default function Display({ text }: DisplayProps) {
+  return (
+    <div className="w-1/2 h-1/2 p-2 bg-white shadow-lg rounded-lg text-2xl">
+      {text}
+    </div>
+  );
 }
