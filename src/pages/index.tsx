7d14cdf512f70bc525d5be878a1a31b713a55386
gabriellukke <gabriellukke@outlook.com>
2022-10-12 21:30:33 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,92 @@
 import type { NextPage } from 'next';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import TextArea from '../components/TextArea';
+import Display from '../components/Display';
+import useDebounce from '../hooks/useDebouncer';
 import { getLanguages, translate } from '../services/translateApi';
 
+type Languages = {
+  code: string;
+  name: string;
+};
+
 const Home: NextPage = () => {
+  const [text, setText] = useState('');
+  const [translation, setTranslation] = useState('');
+  const [languages, setLanguages] = useState<Languages[]>([]);
+  const [sourceLanguage, setSourceLanguage] = useState('en');
+  const [targetLanguage, setTargetLanguage] = useState('en');
+
+  const debounceTranslation = useDebounce(text, 500);
+
   useEffect(() => {
     const fetchLanguages = async () => {
-      const languages = await getLanguages();
-      console.log(languages);
+      const response = await getLanguages();
+      setLanguages(response);
     };
     fetchLanguages();
-    const translateTest = async () => {
-      const translated = await translate('hello', 'en', 'pt');
-      console.log(translated);
-    };
-    translateTest();
   }, []);
+
+  useEffect(() => {
+    if (debounceTranslation) {
+      const fetchData = async () => {
+        const { translatedText } = await translate(
+          debounceTranslation,
+          sourceLanguage,
+          targetLanguage,
+        );
+        setTranslation(translatedText);
+      };
+      fetchData();
+    }
+  }, [debounceTranslation, sourceLanguage, targetLanguage]);
+
+  const setSourceLanguageHandler = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSourceLanguage(e.target.value);
+  };
+
+  const setTargetLanguageHandler = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setTargetLanguage(e.target.value);
+  };
+
   return (
-    <main>
+    <main className="h-screen w-screen bg-ballblue">
       <Header />
+      <section className="flex flex-col justify-center items-center h-full">
+        <div className="flex w-2/5 h-2/3">
+          <TextArea name="target" value={text} onChange={setText} />
+          <Display text={translation} />
+        </div>
+        <div>
+          <select
+            name="sourceLanguage"
+            id="sourceLanguage"
+            onChange={setSourceLanguageHandler}
+          >
+            {languages?.map((language) => (
+              <option key={language.code} value={language.code}>
+                {language.name}
+              </option>
+            ))}
+          </select>
+          <select
+            name="targetLanguage"
+            id="targetLanguage"
+            onChange={setTargetLanguageHandler}
+          >
+            {languages?.map((language) => (
+              <option key={language.code} value={language.code}>
+                {language.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </section>
     </main>
   );
 };
